73d1e1126abdf370e59ef1e3f2b1a4a1383da5ea
ReadabilityLOL <125707933+ReadabilityLOL@users.noreply.github.com>
2023-12-10 18:51:14 +0000

    added gui py

---
+++ gui.py
@@ -0,0 +1,16 @@
+from textual.app import App
+from textual.widgets import Placeholder
+
+
+class MainApp(App):
+
+    async def on_mount(self) -> None:
+        await self.view.dock(Placeholder(name="header"), edge="top", size=3)
+        await self.view.dock(Placeholder(name="footer"), edge="bottom", size=3)
+        await self.view.dock(Placeholder(name="stats"), edge="left", size=40)
+        await self.view.dock(Placeholder(name="message"), edge="right", size=40)
+        await self.view.dock(Placeholder(name="grid"), edge="top")
+
+if __name__ == "__main__":
+app = MainApp()
+app.run()
